60543690330aa9ea68dfed692ef50688770d4344
lwissel <lennart.wissel@gmx.net>
2014-10-07 15:39:58 +0100

    add input output celsius2fahrenheit conversion

---
+++ fahrenheit.cpp
@@ -0,0 +1,12 @@
+// calculat celsius to fahrenheit
+#include <iostream>
+using namespace std;
+
+int main() {
+	double c,f;
+	cout << "Temperature in Degree" << endl;
+	cin >> c;
+	
+	f = 9./5. + 32.;
+	cout << c << " Celsius = " << f << " Fahrenheit" << endl;
+}
