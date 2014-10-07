b86db9955f5867275f5090dd4ba82b1f7c3ec2f3
lwissel <lennart.wissel@gmx.net>
2014-10-07 16:25:17 +0100

    calculate fourdigit number with nested for

---
+++ fourd.cpp
@@ -0,0 +1,24 @@
+// ex1 ad8 calculate nested fors
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool test (int a, int b, int c, int d) {
+	return ( (1000*a+100*b+10*c+d) == (int)(pow(a,b)*pow(c,d)) );
+}
+
+int main () {
+	int a,b,c,d;
+	
+	for(d = 0; d < 10; d++) {
+		for(c = 0; c < 10; c++) {
+			for(b = 0; b < 10; b++) {
+				for (a = 0; a < 10; a++) {
+					if(test(a,b,c,d)) {
+					cout << "True for abcd = " << a << b << c << d << endl;
+					}
+				}
+			}
+		}
+	}
+}
