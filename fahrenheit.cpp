73fe505ed7defac073a09616df89453d9eb6f395
lwissel <lennart.wissel@gmx.net>
2014-10-07 16:03:16 +0100

    add conversion table

--- fahrenheit.cpp
+++ fahrenheit.cpp
@@ -7,6 +7,16 @@ int main() {
 	cout << "Temperature in Degree" << endl;
 	cin >> c;
 	
-	f = 9./5. + 32.;
+	f = 9./5. * c + 32.;
 	cout << c << " Celsius = " << f << " Fahrenheit" << endl;
+	
+	// Convesion table
+	
+	cout << "Conversion table" << endl;
+	cout << " C   |   F" << endl;
+	for(double i=0; i<=100; i+=10) {
+		f = 9./5. * i + 32.;
+		cout << i << "       " << f << endl;
+	}
+	return 0;
 }
