14908fe13f88390b7db0ad76cb7d0b458a77a4b5
lwissel <lennart.wissel@gmx.net>
2014-10-07 15:23:30 +0100

    add poem

--- myapp.cpp
+++ myapp.cpp
@@ -5,4 +5,7 @@ using namespace std;
 int main() {
 	cout << "Hello there!" << endl;
 	cout << "Hi Adam as well!" << endl;
+	
+	// ad 2, bad example of \n
+	cout << "When in trouble or in doubt\nrun in circles,\nscream and shout.\n";
 }
